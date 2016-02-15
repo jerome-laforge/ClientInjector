070c20e84294ac72208901edf9d5e9fe4ba70dc5
jerome-laforge <jerome.laforge@gmail.Com>
2016-02-15 20:08:40 +0100

    Add renew and rebind

---
+++ src/cmd/ClientInjector/dhcp_request_rebind_state.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"bytes"
+	"dhcpv4"
+	"dhcpv4/option"
+	"fmt"
+	"log"
+	"net"
+	"time"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+)
+
+type requestRebindState struct {
+	dhcpContext
+}
+
+func (self *requestRebindState) do() iState {
+	in := self.packetsource.Packets()
+	// Set up all the layers' fields we can.
+	eth := &layers.Ethernet{
+		SrcMAC:       self.macAddr,
+		DstMAC:       hwAddrBcast,
+		EthernetType: layers.EthernetTypeIPv4,
+	}
+	ipv4 := &layers.IPv4{
+		Version:  4,
+		TTL:      255,
+		Protocol: layers.IPProtocolUDP,
+		SrcIP:    net.IPv4zero,
+		DstIP:    net.IPv4bcast,
+	}
+	udp := &layers.UDP{
+		SrcPort: 68,
+		DstPort: 67,
+	}
+	udp.SetNetworkLayerForChecksum(ipv4)
+
+	request := new(dhcpv4.DhcpPacket)
+	request.Construct(option.DHCPREQUEST)
+	request.SetXid(self.xid)
+	request.SetMacAddr([]byte(self.macAddr))
+
+	opt50 := new(option.Option50RequestedIpAddress)
+	opt50.Construct(self.ipAddr)
+	request.AddOption(opt50)
+
+	opt54 := new(option.Option54DhcpServerIdentifier)
+	opt54.Construct(self.ServerIp)
+	request.AddOption(opt54)
+
+	opt61 := new(option.Option61ClientIdentifier)
+	opt61.Construct(byte(1), self.macAddr)
+	request.AddOption(opt61)
+
+	bootp := &PayloadLayer{
+		contents: request.Raw,
+	}
+
+	for {
+		// send request
+		for err := sentMsg(self.handle, eth, ipv4, udp, bootp); err != nil; {
+			log.Println(self.macAddr, "error sending request", err)
+			time.Sleep(2 * time.Second)
+		}
+
+		var (
+			packet   gopacket.Packet
+			timeout  time.Duration
+			deadline = time.Now().Add(2 * time.Second)
+		)
+
+		for {
+			timeout = deadline.Sub(time.Now())
+			select {
+			case <-time.After(timeout):
+				log.Println(self.macAddr, "timeout")
+				return timeoutRebindState{
+					dhcpContext: self.dhcpContext,
+				}
+			case packet = <-in:
+				linkLayer := packet.Layer(layers.LayerTypeEthernet)
+
+				// Is it for me?
+				if !bytes.Equal([]byte(linkLayer.(*layers.Ethernet).DstMAC), self.macAddr) {
+					// no, ignore this packet.
+					continue
+				}
+
+				appLayer := packet.ApplicationLayer()
+				if appLayer == nil {
+					continue
+				}
+
+				dp, err := dhcpv4.Parse(appLayer.Payload())
+				if err != nil {
+					// it is not DHCP packet...
+					continue
+				}
+				if msgType, err := dp.GetTypeMessage(); err == nil {
+					switch msgType {
+					case option.DHCPACK:
+						self.t0, self.t1, self.t2 = extractAllLeaseTime(dp)
+
+						return sleepState{
+							dhcpContext: self.dhcpContext,
+						}
+					case option.DHCPNAK:
+						log.Println(self.macAddr, "Receive NAK")
+						return discoverState{
+							dhcpContext: self.dhcpContext,
+						}
+					default:
+						log.Println(self.macAddr, fmt.Sprintf("Unexpected message [Excpected: %s] [Actual: %s]", option.DHCPACK, msgType))
+						continue
+					}
+				} else {
+					log.Println(self.macAddr, "Option 53 is missing")
+					continue
+				}
+			}
+		}
+	}
+}
--- src/cmd/ClientInjector/dhcp_sleep_state.go
+++ src/cmd/ClientInjector/dhcp_sleep_state.go
@@ -14,7 +14,55 @@ func (self sleepState) do() iState {
 	log.Println(self.macAddr, "ip", util.ConvertUint32ToIpAddr(self.ipAddr), "sleep until t1", self.t1)
 	time.Sleep(self.t1.Sub(time.Now()))
 
-	return requestSelectState{
+	return requestRenewState{
 		dhcpContext: self.dhcpContext,
 	}
+}
+
+type timeoutRenewState struct {
+	dhcpContext
+}
+
+func (self timeoutRenewState) do() iState {
+	now := time.Now()
+	timeout := self.t2.Sub(now) / 2
+	var nextState iState
+	if timeout < time.Minute {
+		timeout = self.t2.Sub(now)
+		nextState = &requestRebindState{
+			dhcpContext: self.dhcpContext,
+		}
+	} else {
+		nextState = &requestRenewState{
+			dhcpContext: self.dhcpContext,
+		}
+	}
+
+	log.Println(self.macAddr, "ip", util.ConvertUint32ToIpAddr(self.ipAddr), "sleep until ", now.Add(timeout))
+	time.Sleep(timeout)
+	return nextState
+}
+
+type timeoutRebindState struct {
+	dhcpContext
+}
+
+func (self timeoutRebindState) do() iState {
+	now := time.Now()
+	timeout := self.t0.Sub(now) / 2
+	var nextState iState
+	if timeout < time.Minute {
+		timeout = self.t0.Sub(now)
+		nextState = &discoverState{
+			dhcpContext: self.dhcpContext,
+		}
+	} else {
+		nextState = &requestRebindState{
+			dhcpContext: self.dhcpContext,
+		}
+	}
+
+	log.Println(self.macAddr, "ip", util.ConvertUint32ToIpAddr(self.ipAddr), "sleep until ", now.Add(timeout))
+	time.Sleep(timeout)
+	return nextState
 }
